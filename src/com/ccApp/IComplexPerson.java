9df0442b35cfff0d977645878a1fd08fd6f76d34
RahulCod <rahul.gajulajbs@gmail.com>
2016-08-01 04:39:19 -0500

    Update IComplexPerson.java

--- src/com/ccApp/IComplexPerson.java
+++ src/com/ccApp/IComplexPerson.java
@@ -2,6 +2,8 @@ package com.ccApp;
 
 public interface IComplexPerson {
 	
+	/* There can be a special kind of request in future to satisfy the Interface segregation principle we used this */
+	
 	public void addSpecialIntrest();
 
 }
